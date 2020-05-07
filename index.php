8309863bda0d84cf5b0f85690b0175090b1765f1
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-07 18:09:50 +0200

    ex4 partie3 php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$departments = array();
+$departments[80] = 'somme';
+$departments[02] = 'aisne';
+$departments[60] = 'oise';
+$departments[62] = 'pas de calais';
+$departments[59] = 'nord';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex4 partie3 php</title>
+</head>
+<body>
+  <?php
+  echo $departments ;
+  ?>
+</body>
+</html>
