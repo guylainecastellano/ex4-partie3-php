6b53431d23e22f49f8bc79d9fe9beeb85a310730
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 11:26:20 +0200

    ex4 partie3 php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 $departments = array();
-$departments[80] = 'somme';
-$departments[02] = 'aisne';
-$departments[60] = 'oise';
-$departments[62] = 'pas de calais';
-$departments[59] = 'nord';
+$departments['80'] = 'somme'; //attribuer des valeurs au tableau
+$departments['02'] = 'aisne';
+$departments['60'] = 'oise';
+$departments['62'] = 'pas de calais';
+$departments['59'] = 'nord';
+$departments2 = array('80'=>'somme','02' =>'aisne','60' =>'oise', '62' => 'pas de calais', '59' => 'nord'); //2e version
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -13,8 +14,5 @@ $departments[59] = 'nord';
   <title>ex4 partie3 php</title>
 </head>
 <body>
-  <?php
-  echo $departments ;
-  ?>
 </body>
 </html>
